9eea6d9b0f6c0ab79887a322f5d76d8717ac6bfe
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-04 02:06:13 +0100

    feat: preliminary events handling in Form component
    fixing the event for the form html tag with
    ```jsx
    const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
      event.preventDefault();
    
      console.log("summary", summary);
      console.log("imgUrl", imgUrl);
    };
    ```

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,18 +1,41 @@
 import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
-import { FC, memo, useState } from "react";
+import { FC, memo, useState, ChangeEvent, FormEventHandler } from "react";
 
 const Input = styled.input``;
 
 export const Form: FC = memo((): JSX.Element => {
 	const [summary, setSummary] = useState("");
-	const [url, setUrl] = useState("");
+	const [imgUrl, setImgUrl] = useState("");
+
+	const onChangeSummary = (event: ChangeEvent<HTMLInputElement>) => {
+		setSummary(event.target.value);
+	};
+
+	const onChangeImgUrl = (event: ChangeEvent<HTMLInputElement>) => {
+		setImgUrl(event.target.value);
+	};
+
+	const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+		event.preventDefault();
+
+		console.log("summary", summary);
+		console.log("imgUrl", imgUrl);
+	};
 
 	return (
-		<form action="#">
-			<Input value="input di testo" />
-			<Input value="url immagine" />
-			<Input type="button" value="Add Card" disabled={!summary || !url} />
+		<form onSubmit={onSubmit}>
+			<Input
+				placeholder="descriptive text"
+				value={summary}
+				onChange={onChangeSummary}
+			/>
+			<Input
+				placeholder="URL of an image"
+				value={imgUrl}
+				onChange={onChangeImgUrl}
+			/>
+			<Input type="submit" value="Add Card" disabled={!summary || !imgUrl} />
 		</form>
 	);
 });
